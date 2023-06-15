15ec70a88c568b1a47559a7e8aa9974d4313f830
Boris <borissoto@gmail.com>
2023-06-15 16:54:19 -0400

    Add Adapters to List of Art and List of Images

--- app/src/main/java/com/gamla/artbooktesting/view/ArtFragmentFactory.kt
+++ app/src/main/java/com/gamla/artbooktesting/view/ArtFragmentFactory.kt
@@ -3,16 +3,22 @@ package com.gamla.artbooktesting.view
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
 import com.bumptech.glide.RequestManager
+import com.gamla.artbooktesting.adapter.ArtRecyclerAdapter
+import com.gamla.artbooktesting.adapter.ImageRecyclerAdapter
 import javax.inject.Inject
 
 class ArtFragmentFactory @Inject constructor(
-    private val glide : RequestManager
+    private val adapter: ArtRecyclerAdapter,
+    private val glide : RequestManager,
+    private val imageRecyclerAdapter: ImageRecyclerAdapter
 ): FragmentFactory()  {
 
     override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
 
         return when(className){
+            ArtFragment::class.java.name -> ArtFragment(adapter)
             ArtDetailsFragment::class.java.name -> ArtDetailsFragment(glide)
+            ImageApiFragment::class.java.name -> ImageApiFragment(imageRecyclerAdapter)
             else -> super.instantiate(classLoader, className)
         }
     }
