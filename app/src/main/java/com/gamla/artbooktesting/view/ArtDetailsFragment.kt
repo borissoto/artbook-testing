3b71c4ba17b7ace9257e177d6a8478b988c8b01b
Boris <borissoto@gmail.com>
2023-06-17 20:44:29 -0400

    Finish app ready for testing

--- app/src/main/java/com/gamla/artbooktesting/view/ArtDetailsFragment.kt
+++ app/src/main/java/com/gamla/artbooktesting/view/ArtDetailsFragment.kt
@@ -2,31 +2,41 @@ package com.gamla.artbooktesting.view
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.RequestManager
 import com.gamla.artbooktesting.R
 import com.gamla.artbooktesting.databinding.FragmentArtDetailsBinding
+import com.gamla.artbooktesting.util.Status
+import com.gamla.artbooktesting.viewmodel.ArtViewModel
 import javax.inject.Inject
 
 class ArtDetailsFragment @Inject constructor(
     val glide: RequestManager
 ) : Fragment(R.layout.fragment_art_details) {
 
-    private var fragmentBinding : FragmentArtDetailsBinding? = null
+    lateinit var viewModel: ArtViewModel
+
+    private var fragmentBinding: FragmentArtDetailsBinding? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProvider(requireActivity()).get(ArtViewModel::class.java)
+
         val binding = FragmentArtDetailsBinding.bind(view)
         fragmentBinding = binding
+
+        subscribeObserver()
 
         binding.artImageView.setOnClickListener {
             findNavController().navigate(ArtDetailsFragmentDirections.actionArtDetailsFragmentToImageApiFragment())
         }
 
-        val callback = object : OnBackPressedCallback(true){
+        val callback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 findNavController().popBackStack()
             }
@@ -34,6 +44,40 @@ class ArtDetailsFragment @Inject constructor(
 
         requireActivity().onBackPressedDispatcher.addCallback(callback)
 
+        binding.saveButton.setOnClickListener {
+            viewModel.makeArt(
+                binding.nameText.text.toString(),
+                binding.artistText.text.toString(),
+                binding.yearText.text.toString()
+            )
+        }
+
+    }
+
+    private fun subscribeObserver() {
+        viewModel.selectedImagesUrl.observe(viewLifecycleOwner) { url ->
+            fragmentBinding?.let {
+                glide.load(url).into(it.artImageView)
+            }
+        }
+
+        viewModel.insertArtMessage.observe(viewLifecycleOwner) {
+            when (it.status) {
+                Status.SUCCESS -> {
+                    Toast.makeText(requireContext(), "Success", Toast.LENGTH_LONG).show()
+                    findNavController().popBackStack()
+                    viewModel.resetInsertMsg()
+                }
+
+                Status.LOADING -> {
+                    Toast.makeText(requireContext(), it.message ?: "Error", Toast.LENGTH_LONG).show()
+                }
+
+                Status.ERROR -> {
+
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
