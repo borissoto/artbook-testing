515b9baafe3c1a507598e403ac585728d649d17f
Boris <borissoto@gmail.com>
2023-06-26 12:59:14 -0400

    Hilt Testing

--- app/src/androidTest/java/com/gamla/artbooktesting/roomdb/ArtDaoTest.kt
+++ app/src/androidTest/java/com/gamla/artbooktesting/roomdb/ArtDaoTest.kt
@@ -1,34 +1,45 @@
 package com.gamla.artbooktesting.roomdb
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.filters.SmallTest
 import com.gamla.artbooktesting.getOrAwaitValueTest
 import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import javax.inject.Inject
+import javax.inject.Named
 
 @SmallTest
 @ExperimentalCoroutinesApi
+@HiltAndroidTest
 class ArtDaoTest {
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    private lateinit var database: ArtDatabase
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    @Named("testDatabase")
+    lateinit var database: ArtDatabase
+
+//    private lateinit var database: ArtDatabase
     private lateinit var artDao: ArtDao
 
     @Before
     fun setup() {
-        database = Room.inMemoryDatabaseBuilder(
+        /*database = Room.inMemoryDatabaseBuilder(
             ApplicationProvider.getApplicationContext(),
             ArtDatabase::class.java
-        ).allowMainThreadQueries().build()
+        ).allowMainThreadQueries().build()*/
+        hiltRule.inject()
 
         artDao = database.artDao()
     }
---
+++ app/src/debug/java/com/gamla/artbooktesting/HiltTestActivity.kt
@@ -0,0 +1,8 @@
+package com.gamla.artbooktesting
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity() {
+}
